20c5a44cbeb34d8b221c7156be47d7df2312a58c
sadmanahmed <9040618-sadmanahmed@users.noreply.gitlab.com>
2021-07-07 03:54:46 +0600

    feat: Added customer id in users from stripe customer id

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def to_s
+    email
+  end
+
+  after_create do
+    customer = Stripe::Customer.create(email: self.email)
+    update(stripe_customer_id: customer.id)
+  end
+
 end
