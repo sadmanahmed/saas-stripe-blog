e9f7969dead7c6a58c6d5682f72742b999ab3bae
sadmanahmed <9040618-sadmanahmed@users.noreply.gitlab.com>
2021-07-08 03:08:14 +0600

    feat: current user can go to billing portal,update and cancel subscription

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "pricing" => "static_pages#pricing"
   resources :posts
   post 'checkout/create' => 'checkout#create', as: "checkout_create"
+  post "billing_portal/create" => 'billing_portal#create', as: "billing_portal_create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
