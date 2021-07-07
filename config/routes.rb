1ce3f0c38d14e95ddaccd63dad833bbe5e3486e6
sadmanahmed <9040618-sadmanahmed@users.noreply.gitlab.com>
2021-07-08 02:12:02 +0600

    Feat: Stripe checkout session builded

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'posts#index'
   get "pricing" => "static_pages#pricing"
   resources :posts
+  post 'checkout/create' => 'checkout#create', as: "checkout_create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
