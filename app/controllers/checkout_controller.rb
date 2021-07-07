5e67be4a6118289da17372dbfbe537bd857b8871
sadmanahmed <9040618-sadmanahmed@users.noreply.gitlab.com>
2021-07-08 02:21:30 +0600

    feat: added checkout button for both plan

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,11 +2,12 @@ class CheckoutController < ApplicationController
 
   def create
     @session = Stripe::Checkout::Session.create({
+                                                  customer: current_user.stripe_customer_id,
                                                   success_url: posts_url,
                                                   cancel_url: pricing_url,
                                                   payment_method_types: ['card'],
                                                   line_items: [
-                                                    {price: 'price_1JAMm3DQAtOnTpa0Mmj0QzBz', quantity: 1},
+                                                    {price: params[:price], quantity: 1},
                                                   ],
                                                   mode: 'subscription',
                                                 })
